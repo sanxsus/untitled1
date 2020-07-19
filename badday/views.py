f2880322bce867db7e367a2184b3bfc61b14a4b9
Alexandra <68400552+sanxsus@users.noreply.github.com>
2020-07-19 12:52:32 +0300

    Update views.py

--- badday/views.py
+++ badday/views.py
@@ -1,34 +1,35 @@
+from functools import wraps
+
 from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer, BookbyidSerializer, BookDetailSerializer
 
 
-class BookView(APIView):
-    def get(self, request):
+class BookViewSet(viewsets.ViewSet):
+    def list(self, request):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response({'books': serializer.data})
 
-
-class Bookbyid(APIView):
-    def article_id(self, request, id):
+    def book_id(self, request, id):
         result = Book.objects.filter(id=id)
         serializer1 = BookbyidSerializer(result, many=True)
         return Response({"result": serializer1.data})
 
     def get(self, request, id):
-        return self.article_id(self, id)
+        return self.book_id(self, id)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
 
 
 class BookCreateView(generics.CreateAPIView):
     serializer_class = BookDetailSerializer
+
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookDetailSerializer
