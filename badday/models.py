b0a32905752c92084d0573cc39731befb2fa55ee
Alexandra <68400552+sanxsus@users.noreply.github.com>
2020-07-19 12:52:04 +0300

    Update models.py
    1

--- badday/models.py
+++ badday/models.py
@@ -6,7 +6,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    id = models.AutoField(primary_key=True, help_text="Unique ID for this particular book across whole library")
 
     def __str__(self):
         return self.title
